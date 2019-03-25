f5dbfea21079cfaa2c46234c220413f05637f9d5
Jemaine Cole <jemainecole@live.com>
2019-03-25 21:12:58 +0200

    new learning

---
+++ c#/kudvenkat/3.mvcDemo/mvcDemo/mvcDemo/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvcDemo.Controllers
+{
+    //http://localhost/mvcDemo/
+
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "Hello from MVC Application";
+        }
+
+        public string getDetails()
+        {
+            return "getDetails() invoked";
+        }
+
+    }
+}
