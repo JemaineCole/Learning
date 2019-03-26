48a0fa86f42005d9caf81319ed6a0ba738833698
Jemaine Cole <jemainecole@live.com>
2019-03-26 23:02:25 +0200

    new crud application

---
+++ asp.net/aspDotNetCrud/aspDotNetCrud/Contact.aspx
@@ -0,0 +1,36 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Contact.aspx.cs" Inherits="aspDotNetCrud.Contact" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <p>
+            EmployeeID<asp:TextBox ID="txtEmployeeID" runat="server" style="margin-left: 30px" OnTextChanged="txtEmployeeID_TextChanged"></asp:TextBox>
+        </p>
+        <asp:Button ID="btDisplay" runat="server" OnClick="btDisplay_Click" Text="Display" style="height: 26px" />
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="btClear" runat="server" OnClick="btClear_Click" Text="Clear" />
+        <p>
+            <asp:Label ID="lblSuccessMsg" runat="server" ForeColor="#00FF40"></asp:Label>
+        </p>
+        <p>
+            <asp:Label ID="lblErrorMsg" runat="server" ForeColor="#FF3300"></asp:Label>
+        </p>
+        <p>
+            &nbsp;</p>
+        <p>
+            <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False">
+                <Columns>
+                    <asp:BoundField DataField="Name" HeaderText="Name" SortExpression="Name" />
+                    <asp:BoundField DataField="Mobile" HeaderText="Mobile" SortExpression="Mobile" />
+                    <asp:BoundField DataField="Address" HeaderText="Address" SortExpression="Address" />
+                </Columns>
+            </asp:GridView>
+        </p>
+    </form>
+</body>
+</html>
---
+++ asp.net/aspDotNetCrud/aspDotNetCrud/Contact.aspx.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;                 //use for sql connection
+using System.Data.SqlClient;    //use for sql connection
+
+namespace aspDotNetCrud
+{
+    public partial class Contact : System.Web.UI.Page
+    {
+        //sql connection string
+        SqlConnection sqlCon = new SqlConnection(@"Data Source = (local)\STUDIOJC-PC; Initial Catalog = TSQLV4; Integrated Security = true");
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btClear_Click(object sender, EventArgs e)
+        {
+            //calling Clear function
+            Clear();
+        }
+
+        //defining function for clear button
+        public void Clear()
+        {
+            txtEmployeeID.Text = "";
+
+            lblErrorMsg.Text = "";
+            lblSuccessMsg.Text = "";
+
+        }
+
+        protected void GridView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void GridView1_SelectedIndexChanged1(object sender, EventArgs e)
+        {
+
+        }
+
+        /*protected void btDisplay_Click(object sender, EventArgs e)
+        {
+            //check if sql connection closed then open
+            if (sqlCon.State == ConnectionState.Closed)
+            {
+                sqlCon.Open();
+            }
+
+            SqlCommand sqlCmd = new SqlCommand("getContact", sqlCon);
+            sqlCmd.CommandType = CommandType.StoredProcedure;
+            sqlCmd.Parameters.AddWithValue("@empID", Convert.ToInt32(txtEmployeeID.Text));
+            sqlCmd.ExecuteNonQuery();
+
+            sqlCon.Close();
+            Clear();
+        }*/
+
+    }
+}
